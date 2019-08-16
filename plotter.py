451f12c7bc98c8613004d96fc17e28b51348dc3f
anmoureaux <anatole.moureaux@student.uclouvain.be>
2019-08-16 23:45:51 +0200

    windowInit() definition

--- plotter.py
+++ plotter.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-#tentative d'écriture d'un programme de plotting de polynômes à une variable 1D opérationnel
+#tentative d'écriture d'un programme de plotting de fonctions à une variable 1D opérationnel
+#Anatole Moureaux, aout 2019
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import *
 
-def main():
 
+#initialisation de la fenêtre et de ses composantes
+def windowInit():
+    
     fen = Tk()
     fen.title("Plot (v1.0)")
 
@@ -17,7 +21,7 @@ def main():
             pad=3
             self._geom='200x200+0+0'
             master.geometry("{0}x{1}+0+0".format(master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
-            master.bind('<Escape>',self.toggle_geom)           
+            master.bind('<Escape>',self.toggle_geom)
         def toggle_geom(self,event):
             geom=self.master.winfo_geometry()
             print(geom,self._geom)
@@ -25,6 +29,7 @@ def main():
             self._geom=geom
     app=FullScreenApp(fen)
 
+    
     fen.columnconfigure(0)
     fen.columnconfigure(1)
     fen.columnconfigure(2)
@@ -36,24 +41,49 @@ def main():
     fen.rowconfigure(0,pad=0)
     fen.rowconfigure(1,pad=0)
     fen.rowconfigure(2,pad=0)
-     
+    fen.rowconfigure(3,pad=0)
+    fen.rowconfigure(4,pad=0)
+    fen.rowconfigure(5,pad=20)
+
+    myColor1 = '#78f8ff'
+    myColor2 = '#78cbff'
+    myColor3 = '#75a1ff'
+    myColor4 = '#7575ff'
+    myColor5 = '#445CFF'
+    
     barre1 = Label(fen,text="")
-    barre1.configure(bg="cyan",font=('Courrier',30))
+    barre1.configure(bg=myColor1,font=('Courrier',30))
     barre1.grid(row=0,column=0,columnspan=3,sticky=EW)
     barre2 = Label(fen,text="")
-    barre2.configure(bg="cyan",font=('Courrier',30))
+    barre2.configure(bg=myColor1,font=('Courrier',30))
     barre2.grid(row=0,column=3,columnspan=4,sticky=EW)
 
     title = Label(fen,text="Plot")
-    title.configure(font=('Courrier',30),bg='cyan')
-    title.grid(row=0,column=3,columnspan = 1,sticky=EW)    
+    title.configure(font=('Courrier',30),bg=myColor1)
+    title.grid(row=0,column=3,columnspan = 1,sticky=EW)
+
     greatbarre1=Label(fen,text="")
-    greatbarre1.configure(bg="blue",font=('Courrier',20))
+    greatbarre1.configure(bg=myColor2,font=('Courrier',20))
     greatbarre1.grid(row=1,column=0,columnspan=7,sticky=EW)
     greatbarre2=Label(fen,text="                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               ")
-    greatbarre2.configure(bg="black",font=('Courrier',10))
+    greatbarre2.configure(bg=myColor3,font=('Courrier',10))
     greatbarre2.grid(row=2,column=0,columnspan=7,sticky=EW)
+
+    greatbarre3=Label(fen,text="")
+    greatbarre3.configure(bg=myColor4,font=('Courrier',5))
+    greatbarre3.grid(row=3,column=0,columnspan=7,sticky=EW)
+
+    greatbarre4=Label(fen,text="")
+    greatbarre4.configure(bg=myColor5,font=('Courrier',2))
+    greatbarre4.grid(row=4,column=0,columnspan=7,sticky=EW)
+
+    funConsigne = Label(fen,text="Écrivez ici la fonction à afficher (sous le format y=f(x)) : ")
+    funConsigne.configure(font=('Courrier',20),bg = myColor3)
+    funConsigne.grid(row=5,column = 0,columnspan=7,sticky=W)
+   
+    return fen
     
-    fen.mainloop()
+def main():
+    windowInit().mainloop()
 
 main()
